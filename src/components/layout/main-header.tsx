99645a6c2e2ad747b8fb3c273e63dbeb7dcf3f8b
Aleks Rūtiņš <aleks@rutins.com>
2024-04-25 13:55:09 -0400

    use image correctly

--- src/components/layout/main-header.tsx
+++ src/components/layout/main-header.tsx
@@ -6,7 +6,7 @@ export default function MainHeader() {
     return (
         <header className="border-b p-6 bg-white flex flex-col gap-2 justify-center items-center relative z-50">
             <Link className="font-display text-lg p-0 m-0" href="/">
-                <Image src={masthead} alt="The Bulldog Bulletin" className="h-8 w-auto"/>
+                <Image src={masthead} alt="The Bulldog Bulletin" width="247" height="32" placeholder='blur'/>
             </Link>
         </header>
     )
