d238b973a2bc43fbae6fc4d3c298fb767d005a7f
Aleks Rūtiņš <aleks@rutins.com>
2024-04-25 13:53:04 -0400

    best practices

--- src/components/layout/main-header.tsx
+++ src/components/layout/main-header.tsx
@@ -1,12 +1,13 @@
 import masthead from '@/app/masthead.png';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function MainHeader() {
     return (
         <header className="border-b p-6 bg-white flex flex-col gap-2 justify-center items-center relative z-50">
-            <a className="font-display text-lg p-0 m-0" href="/">
+            <Link className="font-display text-lg p-0 m-0" href="/">
                 <Image src={masthead} alt="The Bulldog Bulletin" className="h-8 w-auto"/>
-            </a>
+            </Link>
         </header>
     )
 }
